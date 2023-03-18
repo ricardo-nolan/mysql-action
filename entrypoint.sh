ddd4bba538be9e021cc375d21a9f266ce668a255
Ricardo Nolan <1875944+ricardo-nolan@users.noreply.github.com>
2023-03-18 08:38:08 +0000

    debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,5 +28,6 @@ fi
 
 docker_run="$docker_run -d -p $INPUT_HOST_PORT:$INPUT_CONTAINER_PORT mysql:$INPUT_MYSQL_VERSION --port=$INPUT_CONTAINER_PORT"
 docker_run="$docker_run --character-set-server=$INPUT_CHARACTER_SET_SERVER --collation-server=$INPUT_COLLATION_SERVER"
+echo "$docker_run"
 
 sh -c "$docker_run"
